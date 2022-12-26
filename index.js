1b93a7860794d002eee660f591ae736695294443
andreiraduglavan <74043257+andreiraduglavan@users.noreply.github.com>
2022-12-27 00:20:06 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+import author from "./author"
+import blockContent from "./blockContent"
+import category from "./category"
+import post from "./post"
+import siteConfig from "./siteConfig"
+
+export const schemaTypes = [author, blockContent, category, post, siteConfig]
